cd541cfe483b00bad2e3b08ab1c989669822f4b4
BrayanHernandez012 <brayanhdezsanjuan@gmail.com>
2026-07-15 15:14:28 -0400

    Add initial DNA sequence analyser
    Add the first working version of the DNA sequence analyser with sequence validation, nucleotide counting, and GC content calculation

---
+++ dna_analysis.py
@@ -0,0 +1,14 @@
+﻿# This is the first bioinformatics script I have written.
+
+seq = input("Enter a sequence ").upper().strip()
+if seq == "":
+    print("Please enter a valid sequence")
+
+elif any(char not in "ACTG" for char in seq):
+    print("Please enter a valid sequence")
+else:
+    print("=== BASIC ANALYSIS OF DNA SEQUENCES ===")
+    print("Sequence:", seq)
+    print("Length:", len(seq))
+    print("A:", seq.count("A"), "T:", seq.count("T"), "G:", seq.count("G"), "C:", seq.count("C"))
+    print("GC%:", round((seq.count("G") + seq.count("C")) / len(seq) * 100, 2))
