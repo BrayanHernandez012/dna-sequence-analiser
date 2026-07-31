941a1f5e8922b8dfd50684180698a05dd8854c2a
Brayan Hernández San Juan <brayanhdezsanjuan@gmail.com>
2026-07-31 18:49:52 -0400

    Add loops to improve DNA sequence analysis
    Implement loops to prompt the user until a valid sequence is provided

--- dna_analysis.py
+++ dna_analysis.py
@@ -1,14 +1,17 @@
-﻿# This is the first bioinformatics script I have written.
+# This is the first bioinformatics script I have written.
+def analyser():
+     global seq
+     print("=== BASIC ANALYSIS OF DNA SEQUENCES ===")
+     print("Sequence:", seq)
+     print("Length:", len(seq))
+     print("A:", seq.count("A"), "T:", seq.count("T"), "G:", seq.count("G"), "C:", seq.count("C"))
+     print("GC%:", round((seq.count("G") + seq.count("C")) / len(seq) * 100, 2))
 
-seq = input("Enter a sequence ").upper().strip()
-if seq == "":
-    print("Please enter a valid sequence")
-
-elif any(char not in "ACTG" for char in seq):
-    print("Please enter a valid sequence")
-else:
-    print("=== BASIC ANALYSIS OF DNA SEQUENCES ===")
-    print("Sequence:", seq)
-    print("Length:", len(seq))
-    print("A:", seq.count("A"), "T:", seq.count("T"), "G:", seq.count("G"), "C:", seq.count("C"))
-    print("GC%:", round((seq.count("G") + seq.count("C")) / len(seq) * 100, 2))
+seq = input("Enter a sequence: ").strip().upper()
+if seq == "" or any(char not in "ACTG" for char in seq):
+    while seq == "" or any(char not in "ACTG" for char in seq):
+        seq = input("Please enter a valid sequence: ").strip().upper()
+        if seq != "" and not any(char not in "ACTG" for char in seq):
+            analyser()
+else:   
+     analyser()
